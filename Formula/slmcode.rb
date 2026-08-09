0fcda70652d552e1fa211c59e164474f2021c442
Piotr Laczkowski <piotr.laczkowski@gmail.com>
2026-08-09 18:06:38 +0200

    release: update Homebrew formula sha256 for v0.10.1

--- Formula/slmcode.rb
+++ Formula/slmcode.rb
@@ -15,18 +15,18 @@ class Slmcode < Formula
   on_macos do
     on_arm do
       url "https://github.com/UnicoLab/smlcode/releases/download/v#{version}/slmcode-v#{version}-darwin-arm64.tar.gz"
-      sha256 "3716a52537c88c72b06e5af320cdc43d54b8fba2502eb237eed42d612e5189b8"
+      sha256 "992a815002527922b3469c1d3724ae5617e4bfa1243b0add4fce007b55320158"
     end
     on_intel do
       url "https://github.com/UnicoLab/smlcode/releases/download/v#{version}/slmcode-v#{version}-darwin-amd64.tar.gz"
-      sha256 "41b692a0af21e0e1a9c5b73b1d7683aba483b92472f8012f2880db453970e538"
+      sha256 "de3c326a73d38adab89feef5a6721e9166955952482c1f84b717a4e23dd4cc48"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/UnicoLab/smlcode/releases/download/v#{version}/slmcode-v#{version}-linux-amd64.tar.gz"
-      sha256 "d0d25fb63c4e88d240a714c35bc86fa1ce0e788c4629909cd8f1177ca2584146"
+      sha256 "a25ba8edc8df68c70ce46d51b50ebbffc47728b0d628c111d0f0a2aa58d8e4a2"
     end
   end
 
